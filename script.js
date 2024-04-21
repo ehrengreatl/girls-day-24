0ca3acdb316c29b12d2e1a8c2712e10d392818ab
ehrengreatl <147525331+ehrengreatl@users.noreply.github.com>
2024-04-21 22:51:37 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    // Hier wird der Hundeknopf programmiert:
+    $(".buttonDogs").click(function () {
+        // Wähle das Div aus, welches ausgeblendet werden soll
+        $().hide();
+        // Wähle das Div aus, welches angezeigt werden soll
+        $().show();
+        
+        // Wähle den Knopf aus, welches ausgewählt werden soll
+        $().addClass('active');
+        // Wähle die Knöpfe aus, die nicht mehr ausgewählt sind
+        $().removeClass('active');
+        $().removeClass('active');
+    });
+  
+    // Hier wird der Katzenknopf programmiert:
+    $(".buttonCats").click(function () {
+        // Wähle das Div aus, welches ausgeblendet werden soll
+        $().hide();
+        // Wähle das Div aus, welches angezeigt werden soll
+        $().show();
+
+        // Wähle den Knopf aus, welches ausgewählt werden soll
+        $().addClass('active');
+        // Wähle die Knöpfe aus, die nicht mehr ausgewählt sind
+        $().removeClass('active');
+        $().removeClass('active');
+    });
+  
+  
+    // Hier wird der Knopf programmiert, der alles anzeigt:
+    $("buttonAll").click(function () {
+        // Wähle beide Div-Elemente aus, die angezeigt werden sollen
+        $().show();
+        $().show();
+        
+        // Wähle den Knopf aus, welches ausgewählt werden soll
+        $().addClass('active');
+        // Wähle die Knöpfe aus, die nicht mehr ausgewählt sind
+        $().removeClass('active');
+        $().removeClass('active');
+    });
+});
